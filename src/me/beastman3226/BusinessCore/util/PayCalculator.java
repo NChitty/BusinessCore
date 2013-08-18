6844a2fec0bd2c53a6bf65643f04b759e8bda349
John <beastman3226@hotmail.com>
2013-08-18 00:58:45 -0400

    Added equations for calcuting pay

--- src/me/beastman3226/BusinessCore/util/PayCalculator.java
+++ src/me/beastman3226/BusinessCore/util/PayCalculator.java
@@ -1,6 +1,7 @@
 package me.beastman3226.BusinessCore.util;
 
-import me.beastman3226.BusinessCore.Business.Business;
+import me.beastman3226.BusinessCore.business.Business;
+import me.beastman3226.BusinessCore.player.Employee;
 
 /**
  *
@@ -10,6 +11,11 @@ public class PayCalculator {
 
     public static double calculate(Business b, String employee) {
         double pay = b.getNumberOfEmployees()/b.getWorth();
+        if(Employee.getEmployee(b.getEmployeeList()[b.getIndexOf(employee)]).getCompletedJobs() > 0) {
+            pay = pay * (Employee.getEmployee(b.getEmployeeList()[b.getIndexOf(employee)]).getCompletedJobs() * .1);
+        } else if (Employee.getEmployee(b.getEmployeeList()[b.getIndexOf(employee)]).getCompletedJobs() == 0) {
+            pay = pay - (pay*.5);
+        }
         return pay;
     }
 
