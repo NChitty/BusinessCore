15236abd552530b58190c9b368f2c733b3c67809
beastman3226 <beastman3226@hotmail.com>
2013-08-18 21:04:14 -0400

    Changed employeeList and jobList to be stored in vector

--- src/me/beastman3226/BusinessCore/business/Business.java
+++ src/me/beastman3226/BusinessCore/business/Business.java
@@ -1,5 +1,6 @@
 package me.beastman3226.BusinessCore.business;
 
+import java.util.Vector;
 import me.beastman3226.BusinessCore.job.Job;
 import me.beastman3226.BusinessCore.player.Employee;
 import org.bukkit.Bukkit;
@@ -12,8 +13,8 @@ public class Business {
 	private double worth;
 	private final String name;
 	private final String ownerName;
-	private String[] employeeList = new String[100];
-	private Job[] jobList = new Job[100];
+	private Vector<String> employeeList = new Vector(100);
+	private Vector<Job> jobList = new Vector(100);
 
 	public Business(int index, String name, String ownerName) {
 		this.index = index;
@@ -51,54 +52,32 @@ public class Business {
 		return false;
 	}
 
-	public String[] getEmployeeList() {
+	public Vector<String> getEmployeeList() {
 		return employeeList;
 	}
 
-	public void setEmployeeList(String[] employeeList) {
-		this.employeeList = employeeList;
+	public void setEmployeeList(Vector<String> newList) {
+		this.employeeList = newList;
 	}
 
 	public void addEmployee(String employeeName) {
-            this.employeeList[this.getFreeIndex("employeelist")] = employeeName;
+            this.employeeList.add(employeeName);
             Employee emp = new Employee(Bukkit.getPlayer(employeeName), this);
 
 	}
 
-	public Job[] getJobList() {
+	public Vector<Job> getJobList() {
 		return jobList;
 	}
 
-	public void setJobList(Job[] jobList) {
+	public void setJobList(Vector<Job> jobList) {
 		this.jobList = jobList;
 	}
 
 	public void addJob(Job job) {
-		this.jobList[getFreeIndex("joblist")] = job;
+		this.jobList.add(job);
 	}
 
-	public int getFreeIndex(String array) {
-		int index = 0;
-		switch (array.toLowerCase()) {
-			case "employeelist":
-				while(index < this.employeeList.length) {
-					if(this.employeeList[index] == null) {
-						break;
-					} else {
-						index++;
-					}
-				}
-			case "joblist":
-				while(index < this.jobList.length) {
-					if(this.jobList[index] == null) {
-						break;
-					} else {
-						index++;
-					}
-				}
-		}
-		return index;
-	}
 
 	public String getOwnerName() {
 		return ownerName;
@@ -117,25 +96,9 @@ public class Business {
 
     public int getNumberOfEmployees() {
         int number = 0;
-        while(number < this.employeeList.length) {
-            if(this.employeeList[number] == null) {
-                number--;
-                break;
-            }
-            number++;
-        }
+        number = this.employeeList.lastIndexOf(this.employeeList.lastElement());
         return number;
     }
 
-    public int getIndexOf(String employee) {
-        int i = 0;
-        while(i < employeeList.length) {
-            if(employeeList[i].equals(employee)) {
-                break;
-            }
-            i++;
-        }
-        return i;
-    }
 
 }
