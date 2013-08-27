a784b9ffdb3e06aacafd3c0b4e0ad63277663bee
beastman3226 <beastman3226@hotmail.com>
2013-08-27 15:43:55 -0400

    Math stuff

--- src/me/beastman3226/BusinessCore/util/Transconcactor.java
+++ src/me/beastman3226/BusinessCore/util/Transconcactor.java
@@ -18,11 +18,15 @@ public class Transconcactor {
         prices[0] = b.getWorth();
       } else {
          if(isGreaterThan(prices[0], b.getWorth())) {
-           double oldFirst = prices[0];
-           prices = shiftDown(prices, 0);
+           prices = shiftDown(prices, 1);
            prices[0] = b.getWorth();
-         } else if (isLessThan()) {
-
+         } else if (isLessThan(prices[0], b.getWorth())) {
+             for(int j = 0; j < prices.length; j++) {
+                 if(!isLessThan(prices[j], b.getWorth())) {
+                     prices = shiftDown(prices, 1, j);
+                     break;
+                 }
+             }
          }
       }
       i++;
@@ -32,14 +36,15 @@ public class Transconcactor {
   }
 
   public static double[] shiftDown(double[] array, int numberToShift) {
-     int i = 0;
      double[] newArray = new double[array.length];
-     double[] bottomArray = new double[array.length - numberToShift];
-     for(i = 0; i < numberToShift; i++) {
-        newArray[i] = 0.0;
+     double[] bottom = new double[array.length - numberToShift];
+     for(int i = 0; i < numberToShift; i++) {
+         bottom[i] = array[i];
      }
-     for(double d : bottomArray) {
-
+     int i = 0;
+     for(int j = numberToShift; j < newArray.length; j++) {
+         newArray[j] = bottom[i];
+         i++;
      }
      return newArray;
   }
@@ -54,8 +59,27 @@ public class Transconcactor {
     return false;
   }
 
-    private static boolean isLessThan() {
-        throw new UnsupportedOperationException("Not yet implemented");
+    private static boolean isLessThan(double par, double worth) {
+       if(worth > par) {
+           return true;
+       }
+        return false;
+    }
+
+    private static double[] shiftDown(double[] prices, int numberToShift, int startingAt) {
+        double[] newArray = new double[prices.length];
+        double[] bottom = new double[prices.length - numberToShift];
+        int i = 0;
+        for(int k = startingAt; k < numberToShift; k++) {
+            bottom[i] = prices[k];
+            i++;
+        }
+        for(int h = 0; h < newArray.length; h++) {
+            if(h != startingAt) {
+
+            }
+        }
+        return newArray;
     }
 
 }
