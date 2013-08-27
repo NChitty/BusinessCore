b6499488a3f9408a02cff604ea55a9ba37a0f057
beastman3226 <nicholas.chitty@gmail.com>
2013-08-27 14:31:00 -0400

    Bunch of math stuff
    Remind to fix and test this stuff

--- src/me/beastman3226/BusinessCore/util/Transconcactor.java
+++ src/me/beastman3226/BusinessCore/util/Transconcactor.java
@@ -1,4 +1,49 @@
 package me.beastman3226.BusinessCore.util;
 
+import me.beastman3226.BusinessCore.business.*;
+
 public class Transconcactor {
+  
+  public static String[] order(Business[] bList) {
+    String[] list = null;
+    double[] prices = new double[5];
+    int i = 0;
+    for(Business b : bList) {
+      if(i == 0) {
+        list[0] = b.getName();
+        prices[0] = b.getWorth();
+      } else {
+         if(isGreaterThan(prices[0], )) {
+           double oldFirst = prices[0];
+           prices = shiftDown(prices, 0);
+           prices[0] = b.getWorth();
+         } else if (isLessThan()
+      }
+      i++;
+    }
+    
+    return list;
+  }
+  
+  public static double[] shiftDown(double[] array, int numberToShift) {
+     int i = 0;
+     double[] newArray = new double[array.length];
+     double[] bottomArray = new double[array.length - numberToShift];
+     for(i = 0; i < numberToShift; i++) {
+        newArray[i] = null;
+     }
+     for()
+     return newArray;
+  } 
+  
+  /**
+   * @return Returns true if second is greater than first
+   */ 
+  private static boolean isGreater(double first, double second) {
+    if(second > first) {
+      return true;
+    }
+    return false;
+  }
+  
 }
