db3852117be435b2186b936ce0f1e7c863874c35
beastman3226 <beastman3226@hotmail.com>
2013-11-26 07:38:52 -0500

    Stubbed out create command

--- src/me/beastman3226/bc/commands/BusinessCommandHandler.java
+++ src/me/beastman3226/bc/commands/BusinessCommandHandler.java
@@ -1,5 +1,8 @@
 package me.beastman3226.bc.commands;
 
+import me.beastman3226.bc.event.BusinessPreCreatedEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,7 +15,16 @@ public class BusinessCommandHandler implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmnd, String string, String[] args) {
-        //TODO: Business commands
+        if(sender.hasPermission(string)) {
+            if(cmnd.getName().equalsIgnoreCase("b.create") && args.length > 0) {
+                BusinessPreCreatedEvent event = new BusinessPreCreatedEvent(sender, args);
+                Bukkit.getPluginManager().callEvent(event);
+                //TODO: create from args, call post created after
+            }
+        } else {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', cmnd.getPermissionMessage()));
+            return false;
+        }
         return false;
     }
 
