985bf28823cc41448dae751bfaaeca5b4f39e0a9
beastman3226 <beastman3226@hotmail.com>
2013-11-26 07:33:57 -0500

    Added command handlers for user later; split into three to keep organization

---
+++ src/me/beastman3226/bc/commands/BusinessCommandHandler.java
@@ -0,0 +1,19 @@
+package me.beastman3226.bc.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * This class handles all business related commands
+ * @author beastman3226
+ */
+public class BusinessCommandHandler implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmnd, String string, String[] args) {
+        //TODO: Business commands
+        return false;
+    }
+
+}
---
+++ src/me/beastman3226/bc/commands/MiscCommandHandler.java
@@ -0,0 +1,18 @@
+package me.beastman3226.bc.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ * Handles communicative commands and admin/higher level commands
+ * @author beastman3226
+ */
+public class MiscCommandHandler implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
+        return false;
+    }
+
+}
