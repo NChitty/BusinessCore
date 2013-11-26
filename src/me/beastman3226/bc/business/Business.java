2b4130a800dec8d67a729ad78ff0a0329ca987be
beastman3226 <beastman3226@hotmail.com>
2013-11-26 07:07:30 -0500

    Created builder amongst other things for business creation

--- src/me/beastman3226/bc/business/Business.java
+++ src/me/beastman3226/bc/business/Business.java
@@ -1,6 +1,6 @@
 package me.beastman3226.bc.business;
 
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 
 public class Business {
 
@@ -9,13 +9,66 @@ public class Business {
     private String ownerName;
     private double worth;
     private int[] employeeIDs;
-    protected static LinkedHashSet<Business> businessList = new LinkedHashSet<>();
+    protected static HashSet<Business> businessList = new HashSet<>();
 
-    protected Business() {
-
+    private Business(Builder build) {
+        this.id = build.id;
+        this.name = build.name;
+        this.ownerName = build.ownerName;
+        this.worth = build.worth;
+        this.employeeIDs = build.employeeIDs;
     }
 
     public int getID() {
         return id;
+    }
+
+    public static class Builder {
+        private int id;
+        private String name;
+        private String ownerName;
+        private double worth;
+        private int[] employeeIDs;
+
+        public Builder(int id) {
+            this.id = id;
+            this.name = "No Name " + id;
+        }
+
+        public Builder name(String anme) {
+            name = anme;
+            return this;
+        }
+
+        public Builder owner(String owner) {
+            this.ownerName = owner;
+            return this;
+        }
+
+        public Builder balance(double d) {
+            this.worth = d;
+            return this;
+        }
+
+        public Builder ids(int[] ids) {
+            this.employeeIDs = ids;
+            return this;
+        }
+
+        public Builder ids(String[] ids) {
+            int i = 0;
+            int k;
+            this.employeeIDs = new int[ids.length];
+            for(String s : ids) {
+                k = Integer.valueOf(s);
+                this.employeeIDs[i] = k;
+                i++;
+            }
+            return this;
+        }
+
+        public Business build() {
+            return new Business(this);
+        }
     }
 }
--- src/me/beastman3226/bc/business/BusinessManager.java
+++ src/me/beastman3226/bc/business/BusinessManager.java
@@ -1,5 +1,6 @@
 package me.beastman3226.bc.business;
 
+import java.sql.ResultSet;
 import java.util.Iterator;
 
 /**
@@ -7,6 +8,30 @@ import java.util.Iterator;
  * @author beastman3226
  */
 public class BusinessManager {
+
+    /**
+     * This creates a business from a pre-existing business in the database.
+     */
+    public static void createBusiness(ResultSet rs) {
+        //TODO: business database
+    }
+
+    /**
+     * Creates all businesses from file.
+     */
+    public static void createBusinesses() {
+        //TODO: Businesses from file
+    }
+
+    /**
+     * Base method for creating a new business
+     * @param build
+     * @return
+     */
+    public static Business createBusiness(Business.Builder build) {
+        return build.build();
+    }
+
 
     public static Business getBusiness(int id) {
         Business b = null;
