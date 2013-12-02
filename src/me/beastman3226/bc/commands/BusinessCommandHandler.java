3777fc95698b8b4a3f671f4fca1b15e365af4177
beastman3226 <beastman3226@hotmail.com>
2013-12-02 11:25:29 -0500

    Changed from string to cmnd.getPermission(). Other edits

--- src/me/beastman3226/bc/commands/BusinessCommandHandler.java
+++ src/me/beastman3226/bc/commands/BusinessCommandHandler.java
@@ -1,5 +1,8 @@
 package me.beastman3226.bc.commands;
 
+import me.beastman3226.bc.business.Business;
+import me.beastman3226.bc.business.BusinessManager;
+import me.beastman3226.bc.event.BusinessPostCreatedEvent;
 import me.beastman3226.bc.event.BusinessPreCreatedEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -15,12 +18,13 @@ public class BusinessCommandHandler implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmnd, String string, String[] args) {
-        if(sender.hasPermission(string)) {
+        if(sender.hasPermission(cmnd.getPermission())) {
             if(cmnd.getName().equalsIgnoreCase("b.create") && args.length > 0) {
                 BusinessPreCreatedEvent event = new BusinessPreCreatedEvent(sender, args);
                 Bukkit.getPluginManager().callEvent(event);
-                BusinessManager.createBusiness(new Business.Builder(/*TODO: Find next ID*/).name(event.getName()).owner(event.getSender().getName());
-                //TODO: Create and event call
+                Business b = BusinessManager.createBusiness(new Business.Builder(BusinessManager.openID()).name(event.getName()).owner(event.getSender().getName()));
+                BusinessPostCreatedEvent event1 = new BusinessPostCreatedEvent(b);
+                Bukkit.getPluginManager().callEvent(event1);
             }
         } else {
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', cmnd.getPermissionMessage()));
