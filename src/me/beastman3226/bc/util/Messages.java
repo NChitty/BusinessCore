7e937a51993ccfda95c34533f1bfa1ee4a7825de
beastman3226 <beastman3226@hotmail.com>
2013-12-03 07:01:43 -0500

    Created a utility for prefixes

---
+++ src/me/beastman3226/bc/util/Messages.java
@@ -0,0 +1,13 @@
+package me.beastman3226.bc.util;
+
+import org.bukkit.ChatColor;
+
+/**
+ *
+ * @author beastman3226
+ */
+public class Messages {
+
+    public static String PREFIX_ERROR = ChatColor.GRAY + "[" + ChatColor.RED + "BusinessCore" + ChatColor.GRAY + "]: " + ChatColor.WHITE;
+    public static String PREFIX_NOMINAL = ChatColor.GRAY + "[" + ChatColor.AQUA + "BusinessCore" + ChatColor.GRAY + "]: " + ChatColor.WHITE;
+}
