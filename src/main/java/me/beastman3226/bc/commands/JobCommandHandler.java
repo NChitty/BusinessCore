abb14fe064259df589859c1c8c4ed6ba87d22661
beastman3226 <beastman3226@hotmail.com>
2014-01-01 03:04:32 -0500

    Started working on frontend of jobs

--- src/main/java/me/beastman3226/bc/commands/JobCommandHandler.java
+++ src/main/java/me/beastman3226/bc/commands/JobCommandHandler.java
@@ -1,8 +1,11 @@
 package me.beastman3226.bc.commands;
 
+import me.beastman3226.bc.job.JobManager;
+import me.beastman3226.bc.util.Prefixes;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  * Handles commands relating to jobs
@@ -12,7 +15,26 @@ public class JobCommandHandler implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String string, String[] args) {
-        return false;
+        if(cmd.getName().equalsIgnoreCase("j.create") && args.length > 1) {
+            if(sender instanceof Player) {
+                double pay = 0.0;
+                try {
+                    pay = Double.parseDouble(args[0]);
+                } catch (NumberFormatException nfe) {
+                    sender.sendMessage(Prefixes.ERROR + "The first argument must be a number!!");
+                    return false;
+                }
+                String description = "";
+                for(String s : args) {
+                    description = description + " " + s;
+                }
+                JobManager.createJob((Player) sender, description, pay);
+                sender.sendMessage(Prefixes.NOMINAL + "Successfully created job with description: " + description);
+            } else {
+                sender.sendMessage(Prefixes.ERROR + "I need a location to create a job!");
+            }
+        }
+        return true;
     }
 
 }
